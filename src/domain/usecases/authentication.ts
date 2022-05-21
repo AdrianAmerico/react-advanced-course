7992add8fb4123ae89aaba34afd7a47ad15b4a67
AdrianAmerico <1fnadrian1@gmail.com>
2022-05-21 13:36:51 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,7 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { AccountModel } from '../models/account-model'
 
-type AuthenticationParams = {
+export type AuthenticationParams = {
   email: string
   password: string
 };
