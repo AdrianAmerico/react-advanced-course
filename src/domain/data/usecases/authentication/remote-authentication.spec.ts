a5f6a244a8964caa02114104d340f584d8c1cbe0
AdrianAmerico <1fnadrian1@gmail.com>
2022-05-21 11:51:40 -0300

    refactor: nive SYT creation to a factory method

--- src/domain/data/usecases/authentication/remote-authentication.spec.ts
+++ src/domain/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,24 @@
 import { RemoteAuthentication } from "./remote-authentication";
 import { HttPostClientSpy } from "../../protocols/test/mock-http-client";
 
+type SutTypes = {
+  sut: RemoteAuthentication;
+  httpPostClientSpy: HttPostClientSpy;
+};
+
+const makeSut = (url: string = "any_url"): SutTypes => {
+  const httpPostClientSpy = new HttPostClientSpy();
+  const sut = new RemoteAuthentication(url, httpPostClientSpy);
+  return {
+    sut,
+    httpPostClientSpy,
+  };
+};
+
 describe("RemoteAuthentication", () => {
   test("Should call HttpPostClient with correct URL", async () => {
-
-    const url = "any_url";
-    const httpPostClientSpy = new HttPostClientSpy();
-    const sut = new RemoteAuthentication(url, httpPostClientSpy);
+    const url = "other_url";
+    const { sut, httpPostClientSpy } = makeSut(url);
     await sut.auth();
     expect(httpPostClientSpy.url).toBe(url);
   });
