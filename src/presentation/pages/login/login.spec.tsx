05093a560ab6ed152afb518657814db0c1c5cba8
AdrianAmerico <1fnadrian1@gmail.com>
2022-07-06 15:19:10 -0300

    refactor: change login tests to use faker

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render, RenderResult, fireEvent, cleanup } from "@testing-library/react"
 import { ValidationSpy } from "@/presentation/test";
 import Login from "."
+import faker from "@faker-js/faker";
 
 interface SutTypes {
     sut: RenderResult
@@ -31,7 +32,6 @@ describe("Login component", () => {
         const submitButton = sut.getByTestId("submit") as HTMLButtonElement
         expect(submitButton.disabled).toBeTruthy()
 
-
         const emailStatus = sut.getByTestId("email-status")
         expect(emailStatus.title).toBe("Campo obrigatório")
         expect(emailStatus.textContent).toBe("🔴")
@@ -44,18 +44,21 @@ describe("Login component", () => {
     test("should call Validation with correct email", () => {
         const { sut, validationSpy } = makeSut()
         const emailInput = sut.getByTestId("email")
-        fireEvent.input(emailInput, { target: { value: 'any_email' } })
+        const email = faker.internet.email()
+
+        fireEvent.input(emailInput, { target: { value: email } })
 
         expect(validationSpy.fieldName).toBe("email")
-        expect(validationSpy.fieldValue).toBe('any_email')
+        expect(validationSpy.fieldValue).toBe(email)
     })
 
     test("should call Validation with correct password", () => {
         const { sut, validationSpy } = makeSut()
         const passwordInput = sut.getByTestId("password")
-        fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+        const password = faker.internet.password()
+        fireEvent.input(passwordInput, { target: { value: password } })
 
         expect(validationSpy.fieldName).toBe("password")
-        expect(validationSpy.fieldValue).toBe('any_password')
+        expect(validationSpy.fieldValue).toBe(password)
     })
 })
